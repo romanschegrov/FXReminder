f1cb995d2357a1bc81f3b3da7d67c9c4545c58d1
romanschegrov <romanschegrov@gmail.com>
2016-07-06 23:57:25 +0300

    Добавил TitledPane "signIn" для авторизации пользователей.

--- src/main/java/ru/schegrov/controller/AppController.java
+++ src/main/java/ru/schegrov/controller/AppController.java
@@ -1,21 +1,49 @@
 package ru.schegrov.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TitledPane;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 import ru.schegrov.model.AppModel;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AppController {
+public class AppController implements Initializable {
 
     private static final Logger logger = Logger.getLogger(AppController.class);
 
+    private FXMLLoader loader;
     private AppModel model;
+    private ResourceBundle resources;
+
+    @FXML
+    private TitledPane signIn;
+    @FXML
+    private TitledPane jobs;
 
     public AppController(AppModel model) {
         this.model = model;
-        logger.info("AppController init!");
+        logger.info("AppController init");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.resources = resources;
+        initSignController();
+        logger.info("AppController initialized");
+    }
+
+    private void initSignController() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/signin.fxml"));
+            loader.setResources(resources);
+            loader.setControllerFactory(t -> new SignAccordionController());
+            signIn.setContent(loader.load());
+        } catch (IOException e) {
+            logger.error("initSignController error", e);
+        }
     }
 }
---
+++ src/main/java/ru/schegrov/controller/SignAccordionController.java
@@ -0,0 +1,14 @@
+package ru.schegrov.controller;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by ramon on 06.07.2016.
+ */
+public class SignAccordionController {
+    private static final Logger logger = Logger.getLogger(SignAccordionController.class);
+
+    public SignAccordionController() {
+        logger.info("SignAccordionController init");
+    }
+}
